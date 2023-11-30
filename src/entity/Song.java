884acf5c4d0625d2b58c51de39730c3a910e6010
CinquilCinquil <106356391+CinquilCinquil@users.noreply.github.com>
2023-11-30 14:52:14 -0300

    deleted SongFileManager and updated Song

--- src/entity/Song.java
+++ src/entity/Song.java
@@ -1,54 +1,7 @@
 package entity;
 
-import java.io.BufferedInputStream;
-import java.io.FileInputStream;
-import javazoom.jl.player.Player;
-
 public class Song {
 	
-	String name, artist;
-	FileInputStream file;
-	
-	private Player player;
-	
-	public Song(FileInputStream file) {
-		
-		BufferedInputStream bis = new BufferedInputStream(file);
-		
-		try {
-		
-			player = new Player(bis);
-		
-		}
-		catch(Exception e)
-		{
-			
-			System.out.println("Failed to create song!");
-			
-			e.printStackTrace();
-			
-		}
-	}
-	
-	public void play() {
-        
-        try {
-        	
-			player.play();
-			
-		}
-        catch (Exception e) {
-        	
-        	System.out.println("Could not play audio file!");
-			
-			e.printStackTrace();
-		}
-	}
-	
-	public void close() {
-		
-		player.close();
-		
-	}
+	String name, artist, filepath;
 	
 }
